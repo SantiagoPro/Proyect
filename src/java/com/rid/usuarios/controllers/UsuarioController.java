c84965c5276efbbc79abfe0b04c8cbfe30706e4f
Santiago <Santiago@DESKTOP-NI9P794>
2018-05-17 13:52:07 -0500

    Correcciones 17-04-2018

--- src/java/com/rid/usuarios/controllers/UsuarioController.java
+++ src/java/com/rid/usuarios/controllers/UsuarioController.java
@@ -29,35 +29,23 @@ public class UsuarioController implements Serializable {
 
     @EJB
     private UsuarioFacadeLocal ufl;
-    
+
     @EJB
     private TipoDocumentoFacadeLocal tdfl;
 
-    private List<Usuario> usuario; 
+    private List<Usuario> usuario;
     private List<TipoDocumento> tipoDocumento;
-    
+
     private TipoDocumento tipoDocumentoList;
     private Usuario nuevoUsuario;
     private Usuario usuarioSeleccionado;
-    private Long idUsuario;
-    private String nombre;
-    private String apellido;
-    private String sexo;
-    private Date fachaNacimiento;
-    private String seguroMedico;
-    private String rh;
-    private String mail;
-    private String telefono;
-    private String direccion;
-    private String clave;
-    private Short estado;
-    
-    
+    private Usuario user;
+
     public UsuarioController() {
     }
-    
+
     @PostConstruct
-    public void init(){
+    public void init() {
         nuevoUsuario = new Usuario();
     }
 
@@ -77,108 +65,12 @@ public class UsuarioController implements Serializable {
         this.tipoDocumento = tipoDocumento;
     }
 
-    public Long getIdUsuario() {
-        return idUsuario;
-    }
-
-    public void setIdUsuario(Long idUsuario) {
-        this.idUsuario = idUsuario;
-    }
-
     public TipoDocumento getTipoDocumentoList() {
         return tipoDocumentoList;
     }
 
     public void setTipoDocumentoList(TipoDocumento tipoDocumentoList) {
         this.tipoDocumentoList = tipoDocumentoList;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getSexo() {
-        return sexo;
-    }
-
-    public void setSexo(String sexo) {
-        this.sexo = sexo;
-    }
-
-    public Date getFachaNacimiento() {
-        return fachaNacimiento;
-    }
-
-    public void setFachaNacimiento(Date fachaNacimiento) {
-        this.fachaNacimiento = fachaNacimiento;
-    }
-
-    public String getSeguroMedico() {
-        return seguroMedico;
-    }
-
-    public void setSeguroMedico(String seguroMedico) {
-        this.seguroMedico = seguroMedico;
-    }
-
-    public String getRh() {
-        return rh;
-    }
-
-    public void setRh(String rh) {
-        this.rh = rh;
-    }
-
-    public String getMail() {
-        return mail;
-    }
-
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public String getClave() {
-        return clave;
-    }
-
-    public void setClave(String clave) {
-        this.clave = clave;
-    }
-
-    public Short getEstado() {
-        return estado;
-    }
-
-    public void setEstado(Short estado) {
-        this.estado = estado;
     }
 
     public Usuario getNuevoUsuario() {
@@ -192,24 +84,24 @@ public class UsuarioController implements Serializable {
     public Usuario getUsuarioSeleccionado() {
         return usuarioSeleccionado;
     }
-    
-    public List<Usuario> getUsuario(){
-    
+
+    public List<Usuario> getUsuario() {
+
         if (usuario == null || usuario.isEmpty()) {
             usuario = ufl.findAll();
         }
         return usuario;
     }
-    
-    public List<TipoDocumento> getDocumento(){
+
+    public List<TipoDocumento> getDocumento() {
         tipoDocumento = tdfl.findAll();
         return tipoDocumento;
     }
-    
+
     public void seleccionarUsuario(Usuario u) {
         usuarioSeleccionado = u;
     }
-    
+
     public String registrarUsuario() {
         try {
             ufl.create(nuevoUsuario);
@@ -220,7 +112,7 @@ public class UsuarioController implements Serializable {
         }
         return "";
     }
-    
+
     public String eliminarUsuario() {
         try {
             ufl.remove(usuarioSeleccionado);
@@ -241,4 +133,32 @@ public class UsuarioController implements Serializable {
             MessagesUtil.error(null, "Error al editar el usuario.", e.getMessage(), false);
         }
     }
+
+    /*
+    public String bloquearODesbloquear() {
+        try {
+            System.out.println("Vamo a editá el usualio:");
+            System.out.println("Id:" + usuarioSeleccionado.getIdUsuarios());
+            if (usuarioSeleccionado.getEstado() != null) {
+                usuarioSeleccionado.setEstado(
+                        (usuarioSeleccionado.getEstado() == 0) ? Short.valueOf("1") : (usuarioSeleccionado.getEstado() == 1 ? Short.valueOf("0") : usuarioSeleccionado.getEstado()));
+
+            } else {
+                usuarioSeleccionado.setEstado(Short.valueOf("0"));
+            }
+            ufl.edit(usuarioSeleccionado);
+            usuario = null;
+            MessagesUtil.info(null, "Se ha cambiado el destado del usaurio.", "", false);
+        } catch (Exception e) {
+            e.printStackTrace();
+            MessagesUtil.error(null, "Error al cambiar el estado el usuario.", e.getMessage(), false);
+        }
+        usuarioSeleccionado = null;
+        return "listar.xhtml?faces-redirect=true";
+    }
+
+    public String getClassBloqueUsuario(Usuario u) {
+        return ((u.getEstado()) ? "fa-hotel" : ((u.getEstado() == 1) ? "fa-lock" : "fa-unlock"));
+    }
+*/
 }
