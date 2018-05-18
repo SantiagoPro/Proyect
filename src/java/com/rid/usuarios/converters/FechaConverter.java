f1c6e4f060affe52f096fef3ac6d7a67063757c9
karen <karen@10.1.19.179>
2018-05-18 12:49:09 -0500

    18-05-2018

--- src/java/com/rid/usuarios/converters/FechaConverter.java
+++ src/java/com/rid/usuarios/converters/FechaConverter.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.rid.torneos.converters;
+package com.rid.usuarios.converters;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
